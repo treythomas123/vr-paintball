cd3ef85a6ed30d44e8d369b3e589c911a7f9350b
Trey Thomas <trey@treythomas.me>
2017-01-04 22:21:33 -0700

    Add basic paintball model and firing functionality

--- Assets/Scripts/ControllerManager.cs
+++ Assets/Scripts/ControllerManager.cs
@@ -6,6 +6,9 @@ public class ControllerManager : MonoBehaviour {
     private SteamVR_TrackedObject trackedObject;
     private SteamVR_Controller.Device device;
 
+    public Rigidbody paintballPrefab;
+    public Transform firePosition;
+
 	// Use this for initialization
 	void Start () {
         trackedObject = GetComponent<SteamVR_TrackedObject>();
@@ -14,10 +17,12 @@ public class ControllerManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         device = SteamVR_Controller.Input((int)trackedObject.index);
+
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
         {
-            Debug.Log("Trigger Press");
+            Rigidbody paintball;
+            paintball = Instantiate(paintballPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            paintball.AddForce(firePosition.forward * 2000);
         }
-	
 	}
 }

62201c2bfffa113d2c211b846f475e442635e4cc
Trey Thomas <trey@treythomas.me>
2017-01-05 00:10:08 -0700

    Add basic splatter on paintball collision

---
+++ Assets/PaintballWithSplatter/DestroyTimer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTimer : MonoBehaviour {
+
+    void Awake()
+    {
+        Destroy(gameObject, 5);
+    }
+}
---
+++ Assets/PaintballWithSplatter/SplatOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SplatOnCollision : MonoBehaviour {
+
+    public GameObject[] splatTypes;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        
+        Instantiate(
+            splatTypes[Random.Range(0, splatTypes.Length - 1)],
+            collision.contacts[0].point,
+            Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal)
+        );
+    }
+}

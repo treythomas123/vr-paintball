12c9d2d2b115bf7b64a099252ced54d89e5a69eb
Trey Thomas <trey@treythomas.me>
2017-01-08 01:12:38 -0700

    Add barrel fog/smoke effect

--- Assets/Tippmann98/PaintballFirer.cs
+++ Assets/Tippmann98/PaintballFirer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PaintballFirer : MonoBehaviour {
     public Transform firePosition;
     public AudioClip clip;
+    public GameObject smokePrefab;
 
     private AudioSource audioSource;
     
@@ -18,5 +19,8 @@ public class PaintballFirer : MonoBehaviour {
         paintball = Instantiate(paintballPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
         paintball.AddForce(firePosition.forward * 2000);
         audioSource.PlayOneShot(clip);
+
+        var smoke = Instantiate(smokePrefab, firePosition.position, firePosition.rotation) as GameObject;
+        Destroy(smoke, 4);
     }
 }

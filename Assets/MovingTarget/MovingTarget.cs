3e6e2b10430dd641cfffb99576d202ed2480b752
Trey Thomas <trey@treythomas.me>
2017-01-07 22:42:42 -0700

    Add moving target

---
+++ Assets/MovingTarget/MovingTarget.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingTarget : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject firstPoint;
+    public GameObject secondPoint;
+    public float speed;
+
+    private Vector3 goal;
+
+	// Use this for initialization
+	void Start () {
+        target.transform.position = firstPoint.transform.position;
+        goal = secondPoint.transform.position;
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        target.transform.position = Vector3.MoveTowards(target.transform.position, goal, speed * Time.deltaTime);
+        if (target.transform.position == goal) {
+            if (goal == firstPoint.transform.position) {
+                goal = secondPoint.transform.position;
+            } else
+            {
+                goal = firstPoint.transform.position;
+            }
+        }
+    }
+}

a9625811bf41e73466771bd6795030cb976d8fa7
Trey Thomas <trey@treythomas.me>
2017-01-07 22:47:13 -0700

    Clean up splat code

--- Assets/PaintballWithSplatter/SplatOnCollision.cs
+++ Assets/PaintballWithSplatter/SplatOnCollision.cs
@@ -9,15 +9,18 @@ public class SplatOnCollision : MonoBehaviour {
     void OnCollisionEnter(Collision collision)
     {
         var paintball = gameObject;
+        var hit = collision.contacts[0];
+        var otherObject = hit.otherCollider;
+
+        var splatType = splatTypes[Random.Range(0, splatTypes.Length)];
+        var splatPosition = otherObject.ClosestPointOnBounds(hit.point);
+        var splatRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        var splat = Instantiate(splatType, splatPosition, splatRotation) as GameObject;
+
+        // randomly rotate the splat to vary its appearance
+        splat.transform.Rotate(0, Random.Range(0f, 360f), 0);
 
         Destroy(paintball);
-
-        var splat = Instantiate(
-            splatTypes[Random.Range(0, splatTypes.Length)],
-            collision.contacts[0].otherCollider.ClosestPointOnBounds(collision.contacts[0].point),
-            Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal)
-        ) as GameObject;
-        splat.transform.Rotate(0, Random.Range(0f, 360f), 0);
         Destroy(splat, 5);
     }
 }
